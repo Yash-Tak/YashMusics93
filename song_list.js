e647225e1648fdd71cc9cd8994ac4c9bb680fa32
Yash-Tak <yashkumar96100@gmail.com>
2024-06-30 13:18:36 +0530

    added Index.html

---
+++ song_list.js
@@ -0,0 +1,372 @@
+//song list
+let All_song = [
+    {
+      name: "Apa Fer Milaange",
+      path: "music/1.mp3",
+      img: "images/1.jpg",
+      singer: "Made BY Yash Tak"
+    },
+    {
+      name: "Aam Jahe Munda Song",
+      path: "music/2.mp3",
+      img: "images/2.jpg",
+      singer: "Made By Yash"
+    },
+    {
+      name: "Aankhon Mein Tera Hi Chehra",
+      path: "music/3.mp3",
+      img: "images/3.jpg",
+      singer: "Made By Yash"
+    },
+    {
+      name: "Baarish Ki Jaye",
+      path: "music/4.mp3",
+      img: "images/4.jpg",
+      singer: "Made By Yash"
+    },
+    {
+      name: "Bullet Pe Mare Gadiya",
+      path: "music/5.mp3",
+      img: "images/5.jpg",
+      singer: "Made By Yash"
+    },
+    {
+     name: "Kallo",
+     path: "music/6.mp3",
+     img: "images/6.jpg",
+     singer: "Made BY Yash Tak Nohar"
+    },
+    {
+      name: "Veham",
+      path: "music/7.mp3",
+      img: "images/7.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Bhang",
+      path: "music/8.mp3",
+      img: "images/8.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Born To Shine",
+      path: "music/9.mp3",
+      img: "images/9.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Arjan Vaily",
+      path: "music/10.mp3",
+      img: "images/10.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Yaar Ka sataya Hua Hai",
+      path: "music/11.mp3",
+      img: "images/11.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "With you",
+      path: "music/12.mp3",
+      img: "images/12.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "White Brown Black",
+      path: "music/13.mp3",
+      img: "images/13.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Up To U",
+      path: "music/14.mp3",
+      img: "images/14.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Tu Jo Milya",
+      path: "music/15.mp3",
+      img: "images/15.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Teri Baaton Me Aisa Uljha Jiya",
+      path: "music/16.mp3",
+      img: "images/16.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Spellbound",
+      path: "music/17.mp3",
+      img: "images/17.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Softly",
+      path: "music/18.mp3",
+      img: "images/18.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "So High",
+      path: "music/19.mp3",
+      img: "images/19.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Sham Gulabi Song",
+      path: "music/20.mp3",
+      img: "images/20.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Safar",
+      path: "music/21.mp3",
+      img: "images/21.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Rab Ne Chaya Asi Fer Milange",
+      path: "music/22.mp3",
+      img: "images/22.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "One Love",
+      path: "music/23.mp3",
+      img: "images/23.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "No Love",
+      path: "music/24.mp3",
+      img: "images/24.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Nangada",
+      path: "music/25.mp3",
+      img: "images/25.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Haye Re Meri Moto",
+      path: "music/26.mp3",
+      img: "images/26.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Mote Peg",
+      path: "music/27.mp3",
+      img: "images/27.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Mote Peg 2",
+      path: "music/28.mp3",
+      img: "images/28.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Mishri Se Meethi Bata Thari Song",
+      path: "music/29.mp3",
+      img: "images/29.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Minna Minna Song",
+      path: "music/30.mp3",
+      img: "images/30.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Mere Warga",
+      path: "music/31.mp3",
+      img: "images/31.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Matak Chalungi",
+      path: "music/32.mp3",
+      img: "images/32.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Tu Anari Main Khiladi ",
+      path: "music/33.mp3",
+      img: "images/33.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Lover Song",
+      path: "music/34.mp3",
+      img: "images/34.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Love Ya",
+      path: "music/35.mp3",
+      img: "images/35.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Loot Liya",
+      path: "music/36.mp3",
+      img: "images/36.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Kya Loge Tum",
+      path: "music/37.mp3",
+      img: "images/37.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Koka Song",
+      path: "music/38.mp3",
+      img: "images/38.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Khadi Matak",
+      path: "music/39.mp3",
+      img: "images/39.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Kale Kagaz",
+      path: "music/40.mp3",
+      img: "images/40.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Kabza Song",
+      path: "music/41.mp3",
+      img: "images/41.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Jale 2",
+      path: "music/42.mp3",
+      img: "images/42.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },{
+      name: "Hero Honda",
+      path: "music/43.mp3",
+      img: "images/43.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Gpsy Song",
+      path: "music/44.mp3",
+      img: "images/45.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Gulabi Pag",
+      path: "music/46.mp3",
+      img: "images/46.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "295 Song",
+      path: "music/47.mp3",
+      img: "images/47.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Gujjar Haryane Ka",
+      path: "music/48.mp3",
+      img: "images/48.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Fiuji Foujan",
+      path: "music/49.mp3",
+      img: "images/49.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Down To Earth",
+      path: "music/50.mp3",
+      img: "images/50.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Choli Ke Pichhe Song",
+      path: "music/51.mp3",
+      img: "images/51.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Chatak Matak",
+      path: "music/52.mp3",
+      img: "images/53.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Case Song",
+      path: "music/54.mp3",
+      img: "images/54.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Brajil Song",
+      path: "music/55.mp3",
+      img: "images/55.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Bolo Tara ra ",
+      path: "music/56.mp3",
+      img: "images/56.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Tagdi Song",
+      path: "music/57.mp3",
+      img: "images/57.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Bahu Kale Ki ",
+      path: "music/58.mp3",
+      img: "images/58.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+     {
+      name: "Are You Ready",
+      path: "music/59.mp3",
+      img: "images/59.jpg",
+      singer: "Made BY Yash Tak Nohar"
+     },
+ ];
+ /*you can add more song & images from you computer*/
+ 
+ 
+ /*tracks*/
+ let tracks = document.querySelector('.tracks');
+ 
+ //creating a list or generating Html
+ for (let i = 0; i < All_song.length; i++) {
+ 
+   let Html = ` <div class="song">
+       <div class="img">
+       <img src="${All_song[i].img}"/>
+       </div>
+       <div class="more">
+       <audio src="${All_song[i].path}" id="music"></audio>
+       <div class="song_info">
+          <p id="title">${All_song[i].name}</p>
+          <p>${All_song[i].singer}</p>
+       </div>
+       <button id="play_btn"><i class="fa fa-angle-right" aria-hidden="true"></i></button>
+       </div>
+     </div>`;
+ 
+   tracks.insertAdjacentHTML("beforeend", Html);
+ };
+ 
+ 
+ /*please follow all the rules so that you do not face any problem*/
